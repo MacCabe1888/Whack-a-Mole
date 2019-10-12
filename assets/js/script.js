c14c47ee54716ec488422670d2329b369d316f5a
Cameron <cameronjmaccabe@gmail.com>
2019-10-11 22:09:48 -0700

    added game functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
+const scoreboard = document.querySelector(".score");
+const holes = document.querySelectorAll(".hole");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomHole(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+  if (hole == lastHole) return randomHole(holes);
+  lastHole = hole;
+  return hole;
+}
+
+function randomTime(min, max) {
+  return Math.round(min + Math.random() * (max - min));
+}
+
+function peep() {
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  const time = randomTime(200, 1000);
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
+
+function whack(e) {
+  if (!e.isTrusted) return;
+  this.classList.remove("up");
+  score++;
+  scoreboard.textContent = score;
+}
+
+function startGame() {
+  scoreboard.textContent = 0;
+  score = 0;
+  timeUp = false;
+  peep();
+  setTimeout(() => timeUp = true, 10000);
+}
+
+moles.forEach(mole => mole.addEventListener("click", whack));
